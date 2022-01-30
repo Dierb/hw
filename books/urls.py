0c225cabb2fd2f2befe9de3cb7f55d1de8206933
User <dierbadalov@gmail.com>
2022-01-31 00:06:00 +0600

    11122

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
-    path('books/', views.books_all, name='books'),
+    path('books/', views.books_all, name='books_all'),
     path('books/<int:id>/', views.books_detail, name='books_detail'),
+    path('books/<int:id>/update/', views.book_update, name='book_apdate'),
+    path('books/<int:id>/delete/', views.book_delete, name='book_delete'),
+    path('add-book/', views.add_book, name='add_book'),
+    
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
-from . import models
+from . import models, forms
+from django.urls import reverse
 
 # Create your views here.
 
@@ -15,3 +16,33 @@ def books_all(request):
 def books_detail(request, id):
     book = get_object_or_404(models.Book, id = id)
     return render(request, 'books_detail.html',{'book':book})
+
+def add_book(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('book created')
+    else:
+        form = forms.BookForm()
+    return render(request, 'add_book.html', {'form':form})
+
+def book_update(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(instance=book_object, data=request.POST)
+        if form.is_valid():
+           form.save()
+        #    return HttpResponse('book updated')
+        return redirect(reverse("books:books_all"))
+    else:
+        form = forms.BookForm(instance=book_object)
+    return render(request, 'book_update.html', {'form': form, 'object': book_object})
+
+def book_delete(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    book_object.delete()
+    return HttpResponse('book deleted')
+
+
